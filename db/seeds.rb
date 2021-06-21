73745675909c1bba9ba82e856c33e01ec4336d63
Michelle Ranly <meranly8@gmail.com>
2021-06-20 23:08:04 -0500

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Craft.create(name: "Large Wall Hanger", description: "Large pom pom wall hanger with 2 tones of gray", craft_type: "Pom Poms")
+
+Craft.create(name: "Sunset Bracelet", description: "Sunset colors bracelet with buckle; medium length", date_started: "2021-02-10", craft_type: "Bracelet")
+
+Craft.create(name: "Forest Snood", description: "Forest green seed stitched snood; medium size", date_started: "2018-11-01", date_completed: "2018-11-20", price: 25, date_sold: "2018-12-15", craft_type: "Knit", notes: "Sold as Christmas gift")
