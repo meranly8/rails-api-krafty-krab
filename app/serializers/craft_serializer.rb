6cc5c81d714f4016f64f268892a47987d80103d8
Michelle Ranly <meranly8@gmail.com>
2021-06-20 21:10:59 -0500

    Added craft attributes into craft serializer

--- app/serializers/craft_serializer.rb
+++ app/serializers/craft_serializer.rb
@@ -1,4 +1,4 @@
 class CraftSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :name, :description, :date_started, :date_completed, :for_sale, :price, :date_sold, :type, :notes, :image_url
 end
