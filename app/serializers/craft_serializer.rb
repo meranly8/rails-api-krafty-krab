db9a5007f09ca59d92a850cf0cede3e6adf6cf06
Michelle Ranly <meranly8@gmail.com>
2021-06-20 23:05:59 -0500

    Defined attributes for craft_serializer

--- app/serializers/craft_serializer.rb
+++ app/serializers/craft_serializer.rb
@@ -1,4 +1,17 @@
 class CraftSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :date_started, :date_completed, :for_sale, :price, :date_sold, :type, :notes, :image_url
+  
+  attributes :name, :craft_type, :description, :date_started, :date_completed, :price, :date_sold, :notes, :image_url
+  
+  attribute :backlog do |craft|
+    craft.date_started == nil ? true : false
+  end
+
+  attribute :wip do |craft|
+    craft.date_completed == nil && craft.date_started != nil ? true : false
+  end
+
+  attribute :inventory do |craft|
+    craft.date_completed != nil ? true : false
+  end
 end
