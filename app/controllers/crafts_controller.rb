5fea20b846900bf1018c8089695e10b443133186
Michelle Ranly <meranly8@gmail.com>
2021-06-22 01:17:31 -0500

    Defined crafts#create action & #craft_params

--- app/controllers/crafts_controller.rb
+++ app/controllers/crafts_controller.rb
@@ -3,4 +3,18 @@ class CraftsController < ApplicationController
         crafts = Craft.all
         render json: CraftSerializer.new(crafts)
     end
+
+    def create
+        craft = Craft.new(craft_params)
+        if craft.save
+            render json: CraftSerializer.new(craft)
+        else
+            render json: {invalid: "Error occurred on save"}
+        end
+    end
+
+    private
+        def craft_params
+            params.require(:craft).permit(:name, :craft_type, :description, :date_started, :date_completed, :date_sold, :price, :notes, :image_url)
+        end
 end
