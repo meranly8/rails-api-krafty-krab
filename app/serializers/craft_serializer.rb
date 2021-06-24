c55cecdf2c0bab43c27a7912588a649ad4569d5b
Michelle Ranly <meranly8@gmail.com>
2021-06-23 23:01:22 -0500

    Created formatted date attributes & refined :inventory condition

--- app/serializers/craft_serializer.rb
+++ app/serializers/craft_serializer.rb
@@ -3,6 +3,22 @@ class CraftSerializer
   
   attributes :name, :craft_type, :description, :date_started, :date_completed, :price, :date_sold, :notes, :image_url
   
+  attribute :date_started do |craft|
+    craft.date_started != nil ? craft.date_started.strftime("%m/%d/%Y") : nil
+  end
+
+  attribute :date_completed do |craft|
+    craft.date_completed != nil ? craft.date_completed.strftime("%m/%d/%Y") : nil
+  end
+
+  attribute :date_sold do |craft|
+    craft.date_sold != nil ? craft.date_sold.strftime("%m/%d/%Y") : nil
+  end
+
+  attribute :created_at do |craft|
+    craft.created_at.strftime("%m/%d/%Y")
+  end
+  
   attribute :backlog do |craft|
     craft.date_started == nil ? true : false
   end
@@ -12,7 +28,7 @@ class CraftSerializer
   end
 
   attribute :inventory do |craft|
-    craft.date_completed != nil ? true : false
+    craft.date_completed != nil && craft.date_sold == nil ? true : false
   end
 
   attribute :sold do |craft|
